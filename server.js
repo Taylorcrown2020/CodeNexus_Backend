7a9aa4d4def33778017dd9c78b9167dd015185e0
Taylor Crownover <taylorcrownover2020@gmail.com>
2026-01-27 10:21:22 -0600

    update

--- server.js
+++ server.js
@@ -16,16 +16,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: true, // or false depending on your setup
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    }
-});
-
 // Service Packages Definition (same as frontend)
 const servicePackages = {
     'free-basic': {
